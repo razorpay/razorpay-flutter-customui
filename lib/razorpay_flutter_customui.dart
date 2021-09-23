51b297403c77aca27651155ec3554096e63f3e12
Ramprasad A <ramprasad.a@razorpay.com>
2021-09-23 21:42:45 +0530

    - wallet payment added / tested in example app - added getPaymentMethods method - added getAppWhichSupportsUpi method - added initialize SDK method - fixed callback errors

--- lib/razorpay_flutter_customui.dart
+++ lib/razorpay_flutter_customui.dart
@@ -25,19 +25,35 @@ class Razorpay {
   // EventEmitter instance used for communication
   EventEmitter _eventEmitter = EventEmitter();
 
-  Future<String> getPaymentMethods() async {
-    final String paymentMethodsObj =
+  Future<Map<dynamic, dynamic>> getPaymentMethods() async {
+    final Map<dynamic, dynamic> paymentMethodsObj =
         await _channel.invokeMethod('getPaymentMethods');
     return paymentMethodsObj;
   }
 
-  Future<String> getAppsWhichSupportUpi() async {
-    final String paymentMethodsObj =
+  Future<List<String>> getAppsWhichSupportUpi() async {
+    final List<String> paymentMethodsObj =
         await _channel.invokeMethod('getAppsWhichSupportUpi');
     return paymentMethodsObj;
   }
 
-  open(Map<String, dynamic> options) async {
+  Future<String> getCardsNetwork(String cardNumber) async {
+    final String cardNetwork =
+        await _channel.invokeMethod('getCardNetwork', cardNumber);
+    return cardNetwork;
+  }
+
+  Future<bool> isCredAppAvailable() async {
+    final bool isCredAppPresent =
+        await _channel.invokeMethod('isCredAppAvailable');
+    return isCredAppPresent;
+  }
+
+  initilizeSDK(String key) {
+    _channel.invokeMethod('initilizeSDK', key);
+  }
+
+  submit(Map<String, dynamic> options) async {
     Map<String, dynamic> validationResult = _validateOptions(options);
 
     if (!validationResult['success']) {
@@ -51,7 +67,7 @@ class Razorpay {
       return;
     }
 
-    var response = await _channel.invokeMethod('open', options);
+    var response = await _channel.invokeMethod('submit', options);
     _handleResult(response);
   }
 
