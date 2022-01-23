82b462726a8c00df83250de91ad1720de34a4868
NewtaDev <newta.dev@gmail.com>
2022-01-23 09:52:45 +0530

    --fix lint rules

--- lib/razorpay_flutter_customui_web.dart
+++ lib/razorpay_flutter_customui_web.dart
@@ -28,7 +28,6 @@ class RazorpayFlutterCustomuiWeb {
     switch (call.method) {
       case 'getPlatformVersion':
         return getPlatformVersion();
-        break;
       default:
         throw PlatformException(
           code: 'Unimplemented',
